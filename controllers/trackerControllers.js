f952ea4ca50b8bd0669fa458f4b2e44ca83367e0
rezen <rezen.khadgi@gmail.com>
2025-01-22 15:13:27 +0545

    mood calender backend

--- controllers/trackerControllers.js
+++ controllers/trackerControllers.js
@@ -1,52 +1,40 @@
-const generateJwt = require("../utils/generateJwt");
 const { getUserById, Mood } = require("../model/trackerModel");
 
 const getUserInfo = async (req, res) => {
-    try {
+  try {
+    const user = await getUserById(req.user);
+    if (!user) return res.status(404).json({ error: "User not found." });
 
-        const user = await getUserById(req.user);
-        const moods = await Mood.getMoodByUser(user);
-
-        if (!user) {
-            return res.status(404).json("User not found.")
-        }
-
-        res.json(user);
-        res.status(200).json(moods)
-
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json("Server error")
-        res.status(500).json({error: "Failed to get mood."})
-        
-
-    }
-}
+    const moods = await Mood.getMoodByUser(req.user);
+    res.status(200).json({ user_name: user.user_name, moods });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Failed to retrieve user info." });
+  }
+};
 
 const addMood = async (req, res) => {
-    try{
-        const {moodDate, moodEmoji, moodLabel} = req.body;
-        const userId = req.user;
+  try {
+    const { moodDate, moodEmoji, moodLabel } = req.body;
+    const userId = req.user;
 
-        const newMood = await Mood.addMood(userId, moodDate, moodEmoji,moodLabel);
-        res.status(201).json(newMood)
-    }catch(error){
-        console.error(error.message);
-        res.status(500).json({error: "Failed to add mood."});
-    }
-}
+    const newMood = await Mood.addMood(userId, moodDate, moodEmoji, moodLabel);
+    res.status(201).json(newMood);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Failed to add mood." });
+  }
+};
 
 const getMoods = async (req, res) => {
-    try {
-        const userId = req.user;
-
-        const moods = await Mood.getMoodByUser(userId);
-        res.status(200).json(moods)
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json({error: "Failed to get mood."})
-        
-    }
-}
+  try {
+    const userId = req.user;
+    const moods = await Mood.getMoodByUser(userId);
+    res.status(200).json(moods);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Failed to get moods." });
+  }
+};
 
 module.exports = { getUserInfo, addMood, getMoods };
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const { config } = require('dotenv')
+
 config()
 
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
+app.use(cors());
 
 // middleware
 
